348030ab1d96845e83adeab7b4179d4af0970f1e
Andrej Novotny <andrej.novotny97@gmail.com>
2020-10-18 13:11:05 +0200

    Add Linking to Banner's Login and Register Buttons

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,25 @@
-import React from "react";
-import "../styles/styles.css";
-import Button from "./Button";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from './Button';
+import '../styles/styles.css';
 
 export default function Banner() {
 	return (
-		<div id='banner'>
-			<div id='bannerImg'>
+		<div id="banner">
+			<div id="bannerImg">
 				<img
-					src={require("../assets/images/bannerImg.jpg")}
-					alt='Main banner logo'
+					src={require('../assets/images/bannerImg.jpg')}
+					alt="Main banner logo"
 				/>
 			</div>
 			<span>B.E.N. Boch</span>
-			<div id='accessMenu'>
-				<Button title='Log In' />
-				<Button title='Register' />
+			<div id="accessMenu">
+				<Link to="/login">
+					<Button title="Log In" />
+				</Link>
+				<Link to="/register">
+					<Button title="Register" />
+				</Link>
 			</div>
 		</div>
 	);
