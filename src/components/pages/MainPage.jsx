a53cbce72b3f90adba26ee8c84a1705736eb893d
Andrej Novotny <andrej.novotny97@gmail.com>
2020-10-18 14:08:43 +0200

    Add MainPage

---
+++ src/components/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../Banner';
+import Navbar from '../NavBar';
+
+export default function MainPage() {
+	return (
+		<>
+			<Banner />
+			<Navbar />
+		</>
+	);
+}
