478804c82fc4604ed636176ecef6c89dbdb36ec1
Andrej Novotny <andrej.novotny97@gmail.com>
2020-10-19 07:36:49 +0200

    Add tabs to navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import './styles/styles.css';
 import Banner from './components/Banner';
 import NavBar from './components/NavBar';
-import './styles/styles.css';
+import Home from './components/Home';
+import News from './components/News';
+import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
 	return (
 		<>
-			<NavBar />
 			<Banner />
-			<Switch></Switch>
+			<NavBar />
+			<Switch>
+				<Route path="/login" component={LoginForm} />
+				<Route path="/register" component={RegisterForm} />
+				<Route path="/home" component={Home} />
+				<Route path="/news" component={News} />
+				<Route path="/contact" component={Contact} />
+				<Route path="/about" component={About} />
+				<Redirect from="/" exact to="/home" />
+			</Switch>
 		</>
 	);
 }
